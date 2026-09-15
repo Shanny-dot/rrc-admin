c8e74175a7b1eb6bac7f7383b441446201376034
Shanny-dot <shantanufadnavis@protonmail.com>
2026-09-16 05:04:02 +0530

    feat: remove Content Management section from admin portal

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ import Login from './pages/Login';
 import Support from './pages/Support';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import TermsOfService from './pages/TermsOfService';
-import Content from './pages/Content';
 import Sidebar from './components/Sidebar';
 
 const ProtectedRoute = ({ children, session }) => {
@@ -61,7 +60,6 @@ function App() {
         <Route path="/rewards" element={<ProtectedRoute session={session}><Rewards /></ProtectedRoute>} />
         <Route path="/redemptions" element={<ProtectedRoute session={session}><Redemptions /></ProtectedRoute>} />
         <Route path="/transactions" element={<ProtectedRoute session={session}><Transactions /></ProtectedRoute>} />
-        <Route path="/content" element={<ProtectedRoute session={session}><Content /></ProtectedRoute>} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/TermsOfService.jsx
+++ src/pages/TermsOfService.jsx
@@ -1,3 +1,4 @@
+
 import { ArrowLeft, FileText } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 
